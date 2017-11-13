87fa4ca93dc74a28af64ec547c2decf92a0d817c
ormelaver <33628142+ormelaver@users.noreply.github.com>
2017-11-13 16:05:51 +0200

    Add files via upload

---
+++ multicast/handler.js
@@ -0,0 +1,125 @@
+"use strict";
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient({region: "us-east-1"});
+const uuid = require("uuid/v1");
+const eventList = require("event_list");
+const utils = require("utils");
+
+
+module.exports.postEvent = (event, context, callback) => {
+  const currentId = uuid();
+  try {
+    const eventName = Object.keys(event)[0];
+    const dataToSend = event[eventName];
+    const params = {
+      Item: {
+          id: currentId,
+          eventName: eventName,
+          data: dataToSend
+      },
+      TableName: "events"
+  };
+
+  if (eventName == null) {
+    callback(null, utils.createResponse(400, "Event name must not be empty!", ""));
+  }
+  docClient.put(params, (err, data) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      utils.publish(eventName, dataToSend);
+      callback(null, utils.createResponse(200, "Your event was logged with id", currentId));
+    }
+  });
+  } catch(err) {
+    if (err) {
+      callback(err, null);  
+    }
+  }
+};
+
+
+module.exports.putEvent = (event, context, callback) => {
+  const currentId = event.id;
+  const newData = event.payload.data;
+  const params = {
+   TableName : "events",
+      Key : {
+        "id" : currentId
+      },
+    UpdateExpression : "SET #attrName = :attrValue",
+    ConditionExpression : "#id = :id",
+    ExpressionAttributeNames : {
+        "#attrName" : "data",
+        "#id" : "id"
+    },
+    ExpressionAttributeValues : {
+        ":attrValue" : newData, 
+        ":id" : currentId
+    }
+
+  };
+  try {
+  docClient.update(params, (err, data) => {
+    if (err) {
+      callback(null, utils.createResponse(404, "Item not found", ""));
+    } else {
+      callback(null, utils.createResponse(200, "Item was updated with new data: ", newData));
+    }
+  });
+  } catch(err) {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, response);  
+    }
+  }
+
+
+};
+
+module.exports.getEvent = (event, context, callback) => {
+  const currentId = event.id;
+   var params = {
+   TableName : "events",
+   KeyConditionExpression: "id = :eventId",
+   ExpressionAttributeValues: {
+       ":eventId": currentId
+   }
+  };
+
+ 
+ docClient.query(params, (err, data) => {
+   if (err) {
+      callback(err, null);
+   }else {
+      callback(null, data.Items); 
+    }
+  });
+}
+
+module.exports.deleteEvent = (event, context, callback) => {
+  const currentId = event.id;
+  var params = {
+   TableName : "events",
+   Key : {
+        "id" : currentId
+      },
+   ConditionExpression: "id = :eventId",
+   ExpressionAttributeValues: {
+       ":eventId": currentId
+   }
+  };
+  docClient.delete(params, (err, data) => {
+    if (err) {
+      callback(null, utils.createResponse(404, "Item was not found! deletion unsuccessful", ""));
+    }
+    else {
+      callback(null, utils.createResponse(200, "Item was deleted successfully", ""));
+    }
+  });
+}
+
+   
+
+
---
+++ multicast/utils.js
@@ -0,0 +1,96 @@
+const QUEUE_URL = "https://sqs.us-east-1.amazonaws.com/622578823477/eventQueue.fifo";
+const AWS = require("aws-sdk");
+const sqs = new AWS.SQS({region : 'us-east-1'});
+const eventList = require("event_list");
+const sns = new AWS.SNS();
+
+// module.exports.Enqueue = function(event, context) {
+// 	console.log("ENQUEUE ENQUEUE ENQUEUE ENQUEUE");
+//   const params = {
+//     MessageBody: JSON.stringify(event),
+//     QueueUrl: QUEUE_URL
+//   };
+//   sqs.sendMessage(params, (err,data) => {
+//     if(err) {
+//       console.log('error:',"Failed to send message" + err);
+//       context.done('error', "ERROR Put SQS");  // ERROR with message
+//     }else{
+//       console.log('data:',data.MessageId);
+//       context.done(null,'');  // SUCCESS 
+//     }
+//   });
+// }
+
+// const sendByType = {
+// 	ip: (ip, data) => {
+// 		send(ip, data);
+// 	},
+// 	url: (url, data) => {
+// 	  send(url, data);
+// 	},
+// 	iphone: (number, data) => {
+// 		send(number, data);
+// 	}
+// }
+
+
+// module.exports.Dequeue = () => {
+//     const params = {
+//                 QueueUrl : QUEUE_URL
+//             }
+//     const message = sqs.receiveMessage(params)
+//     if (sendByType[message.type] === undefined) {
+// 				console.log('this type does not exist: ' + message.type);
+// 		} else {
+// 				const eventName = Object.keys(message.body)[0];
+//         const subscribers = eventList[eventName];
+//         for (var i = 0; i < Things.length; i++) {
+//         	Things[i]
+//         }
+//         	sendByType[subscribers[i][type]](message.body.access, data);	
+// 		}
+//  };
+
+module.exports.publish = function(eventName, data) {
+ 	var params = {
+  Message: data, /* required */
+  TopicArn: 'arn:aws:sns:us-east-1:622578823477:' + eventName
+	};
+		sns.publish(params, function(err, data) {
+  		if (err){
+  			console.log(err, err.stack);
+  		} 
+  		else {
+  		console.log(data);
+  		} 
+		});
+ }
+
+module.exports.getElementById = function(id) {
+ var params = {
+   TableName : "events",
+   KeyConditionExpression: "id = :eventId",
+   ExpressionAttributeValues: {
+       ":eventId": id
+   }
+  };
+
+
+   docClient.query(params, function(err, data){
+    if(err){
+      return err;
+    }else{
+      return data;
+       }
+   });
+}
+
+module.exports.createResponse = function(code, comment, auxiliary) {
+	return response = {
+      statusCode: code,
+      body: {
+      message: comment + " " + auxiliary,
+      },  
+  };
+}
+
